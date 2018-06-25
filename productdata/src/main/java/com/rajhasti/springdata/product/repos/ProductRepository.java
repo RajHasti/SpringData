a636deb34dd42e22cc04addff97848f0a788fbd5
RajHasti <rajglobe@gmail.com>
2018-06-25 17:11:58 -0400

    jpql learnings

--- productdata/src/main/java/com/rajhasti/springdata/product/repos/ProductRepository.java
+++ productdata/src/main/java/com/rajhasti/springdata/product/repos/ProductRepository.java
@@ -1,12 +1,13 @@
 package com.rajhasti.springdata.product.repos;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.rajhasti.springdata.product.entity.Product;
 import java.lang.String;
 import java.util.List;
 
-public interface ProductRepository extends CrudRepository<Product, Integer> {
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
 
 	List<Product> findByName(String name);
 
--- productdata/src/test/java/com/rajhasti/springdata/product/ProductdataApplicationTests.java
+++ productdata/src/test/java/com/rajhasti/springdata/product/ProductdataApplicationTests.java
@@ -12,6 +12,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.rajhasti.springdata.product.entity.Product;
@@ -34,10 +39,10 @@ public class ProductdataApplicationTests {
 	@Test
 	public void testCreate() {
 		Product product = new Product();
-		product.setId(1);
-		product.setName("Iphone");
-		product.setDesc("Awesome");
-		product.setPrice(1000d);
+		//product.setId(1);
+		product.setName("Laptop");
+		product.setDesc("Dell");
+		product.setPrice(2500d);
 
 		repository.save(product);
 	}
@@ -75,49 +80,76 @@ public class ProductdataApplicationTests {
 		System.out.println("Count ----->" + repository.count());
 	}
 
-
 	@Test
 	public void testFindByName() {
-	
+
 		List<Product> products = repository.findByName("IWatch");
 		products.forEach(p -> System.out.println(p.getPrice()));
 
 		List<Product> products1 = repository.findByName("IWatch");
 		products1.forEach(p -> System.out.println(p.getPrice()));
 	}
+
 	@Test
 	public void testFindByNameAndDesc() {
 		List<Product> products = repository.findByNameAndDesc("tv", "samsung");
 		products.forEach(p -> System.out.println(p.getDesc()));
 		products.forEach(p -> System.out.println(p.getPrice()));
 	}
+
 	@Test
 	public void testFindByPriceGreaterthan() {
 		List<Product> products = repository.findByPriceGreaterThan(1000d);
 		products.forEach(p -> System.out.println(p.getName()));
 		products.forEach(p -> System.out.println(p.getPrice()));
 	}
+
 	@Test
 	public void testFindByDesc() {
 		List<Product> products = repository.findByDescContains("dell");
 		products.forEach(p -> System.out.println(p.getPrice()));
 	}
+
 	@Test
 	public void testFindByPriceBetween() {
 		List<Product> products = repository.findByPriceBetween(1000d, 5000d);
 		products.forEach(p -> System.out.println(p.getName()));
-	
+
 	}
+
 	@Test
 	public void testFindByDescLike() {
 		List<Product> products = repository.findByDescLike("%de%");
 		products.forEach(p -> System.out.println(p.getName()));
-	
+
 	}
+
 	@Test
 	public void testFindByIdIn() {
-		List<Product> products = repository.findByIdIn(Arrays.asList(1,2,4));
+		List<Product> products = repository.findByIdIn(Arrays.asList(1, 2, 4));
 		products.forEach(p -> System.out.println(p.getName()));
-	
+
 	}
+
+	@Test
+	public void testFindAllPaging() {
+		Pageable pageable = new PageRequest(2, 1);
+		Page<Product> results = repository.findAll(pageable);
+		results.forEach(p -> System.out.println(p.getName()));
+	}
+
+	@Test
+	public void testFindAllSorting() {
+
+		repository.findAll(new Sort(Direction.DESC, "name")).forEach(p -> System.out.println(p.getName()));
+		repository.findAll(new Sort(Direction.DESC, "name","price")).forEach(p -> System.out.println(p.getName()));
+
+		repository.findAll(new  Sort(new Sort.Order(Direction.DESC, "name"),new Sort.Order("price")) ).forEach(p -> System.out.println(p.getName()));
+	}
+	@Test
+	public void testFindAllPagingAndSorting() {
+		Pageable pageable = new PageRequest(1,2,Direction.DESC,"name");
+				repository.findAll(pageable).forEach(p -> System.out.println(p.getName()));
+	}
+	
 }
