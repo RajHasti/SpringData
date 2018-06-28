14112e4a4360f40ae42807e5e5cc5e07e58c69cb
RajHasti <rajglobe@gmail.com>
2018-06-28 14:45:40 -0400

    updated with Alter table to add address details

--- customer/customersql.sql
+++ customer/customersql.sql
@@ -4,13 +4,28 @@ GO
 
 If not exists( select [name] from sys.tables where[name]='dbo.customer')
 	CREATE TABLE dbo.customer  
-	   (id int  NOT NULL ,  
+	   (id int  NOT NULL IDENTITY(1,1) ,  
 		name varchar(100) NOT NULL,  
 		email varchar(100) NOT NULL
 	  CONSTRAINT customerid_pk PRIMARY KEY (id))
 GO  
 
 select * from customer;
+ select
+        count(*) as col_0_0_ 
+    from
+        customer customer0_
+
+	use master	
+
+ALTER TABLE dbo.customer
+  ADD streetaddress varchar(30),
+		city varchar(20) ,
+		state varchar(20) ,
+		zipcode varchar(20) ,
+		country varchar(20) ;
+
+	go
 
 
 
